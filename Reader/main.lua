3e5734768e971846589faf45961d91ad058f0e5f
Diefonk <thediefonk@gmail.com>
2021-06-08 21:24:15 +0200

    Now possible to load and display one file by drawing it to an image

--- Reader/main.lua
+++ Reader/main.lua
@@ -1,31 +1,45 @@
 import "CoreLibs/graphics"
 
+local gfx <const> = playdate.graphics
+
 local y = 0
-local carmilla
+local text
+local image
 local width, height
+local fonts = {
+	[gfx.font.kVariantNormal] = "Sasser-Slab",
+    [gfx.font.kVariantBold] = "Sasser-Slab-Bold",
+    [gfx.font.kVariantItalic] = "Sasser-Slab-Italic"
+}
 
 function init()
-	local filesList = playdate.file.listFiles("/")
-	for index = 1, table.getsize(filesList) do
-		print(filesList[index])
-	end
-	local carmillaFile = playdate.file.open("Carmilla.txt")
-	carmilla = carmillaFile:read(1000000)
-	width, height = playdate.graphics.getTextSizeForMaxWidth(carmilla, 400)
-	playdate.graphics.drawTextInRect(carmilla, 0, 0, width, height)
+	local fontFamily = gfx.font.newFamily(fonts)
+	gfx.setFontFamily(fontFamily)
+	--local filesList = playdate.file.listFiles("/")
+	--for index = 1, table.getsize(filesList) do
+		--print(filesList[index])
+	--end
+	local file = playdate.file.open("text.txt")
+	text = file:read(300000)
+	width, height = gfx.getTextSizeForMaxWidth(text, 390)
+	image = gfx.image.new(400, height + 10, gfx.kColorWhite)
+	gfx.pushContext(image)
+	gfx.drawTextInRect(text, 5, 5, width, height)
+	gfx.popContext()
+	image:draw(0, y)
 end
 
 function playdate.update()
 end
 
 function playdate.cranked(change, accChange)
-	y -= change
+	y -= change * 2
 	if y > 0 then
 		y = 0
+	elseif y < 230 - height then
+		y = 230 - height
 	end
-	--playdate.graphics.clear()
-	--playdate.graphics.drawTextInRect(carmilla, 0, y, width, height)
-	playdate.display.setOffset(0, y)
+	image:draw(0, y)
 end
 
 init()
