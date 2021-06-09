5dd55ac2f9e94f1828fc43d4b5be0bdef4bc4ab0
Diefonk <thediefonk@gmail.com>
2021-06-09 19:35:12 +0200

    Added d-pad support

--- Reader/main.lua
+++ Reader/main.lua
@@ -1,28 +1,31 @@
 import "CoreLibs/graphics"
+import "CoreLibs/timer"
 
 local gfx <const> = playdate.graphics
+local tmr <const> = playdate.timer
 
 local y = 0
-local text
 local image
-local width, height
-local fonts = {
-	[gfx.font.kVariantNormal] = "Sasser-Slab",
-    [gfx.font.kVariantBold] = "Sasser-Slab-Bold",
-    [gfx.font.kVariantItalic] = "Sasser-Slab-Italic"
-}
+local imageHeight
+local buttonTimer
 
 function init()
-	local fontFamily = gfx.font.newFamily(fonts)
+	local fontPaths = {
+		[gfx.font.kVariantNormal] = "Sasser-Slab",
+    	[gfx.font.kVariantBold] = "Sasser-Slab-Bold",
+    	[gfx.font.kVariantItalic] = "Sasser-Slab-Italic"
+	}
+	local fontFamily = gfx.font.newFamily(fontPaths)
 	gfx.setFontFamily(fontFamily)
 	--local filesList = playdate.file.listFiles("/")
 	--for index = 1, table.getsize(filesList) do
 		--print(filesList[index])
 	--end
 	local file = playdate.file.open("text.txt")
-	text = file:read(300000)
-	width, height = gfx.getTextSizeForMaxWidth(text, 390)
-	image = gfx.image.new(400, height + 10, gfx.kColorWhite)
+	local text = file:read(300000)
+	local width, height = gfx.getTextSizeForMaxWidth(text, 390)
+	imageHeight = height + 10
+	image = gfx.image.new(400, imageHeight, gfx.kColorWhite)
 	gfx.pushContext(image)
 	gfx.drawTextInRect(text, 5, 5, width, height)
 	gfx.popContext()
@@ -30,16 +33,47 @@ function init()
 end
 
 function playdate.update()
+	tmr.updateTimers()
 end
 
-function playdate.cranked(change, accChange)
-	y -= change * 2
+function scroll(change)
+	y -= change
 	if y > 0 then
 		y = 0
-	elseif y < 230 - height then
-		y = 230 - height
+	elseif y < 240 - imageHeight then
+		y = 240 - imageHeight
 	end
 	image:draw(0, y)
+end
+
+function playdate.cranked(change)
+	scroll(change * 2)
+end
+
+function playdate.upButtonDown()
+	if buttonTimer then
+		buttonTimer:remove()
+	end
+	buttonTimer = tmr.keyRepeatTimerWithDelay(1, 1, scroll, -3)
+end
+
+function playdate.downButtonDown()
+	if buttonTimer then
+		buttonTimer:remove()
+	end
+	buttonTimer = tmr.keyRepeatTimerWithDelay(1, 1, scroll, 3)
+end
+
+function playdate.upButtonUp()
+	if buttonTimer then
+		buttonTimer:remove()
+	end
+end
+
+function playdate.downButtonUp()
+	if buttonTimer then
+		buttonTimer:remove()
+	end
 end
 
 init()
